2fcb04990432f72e5c493d3658336f5e9edf1be8
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-25 10:22:42 +0530

    tested functionality of functions

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const birthDate = document.querySelector("#birth-date");
-const luckyNum = document.querySelector("#lucky-number");
-const checkButton = document.querySelector("#check-button");
+const birthDate = document.querySelector("#birth-date"); //Date input
+const luckyNum = document.querySelector("#lucky-number"); //Lucky Number input
+const checkButton = document.querySelector("#check-button"); //Check Button for result.
+
+function getSum(dob) {
+  let sum = 0;
+  for (let i = 0; i < dob.length; i++) {
+    sum += Number(dob.charAt(i));
+  }
+  return sum;
+}
+
+function valueCheck(dob) {
+  dob = dob.value;
+  dob = dob.replaceAll("-", "");
+  return dob;
+}
+
+function checkLucky(sumOfDate, luckyNumValue) {
+  if (sumOfDate % luckyNumValue === 0) {
+    return "You are lucky :D";
+  } else {
+    return "you are unlucky , sadge";
+  }
+}
 
 function checkEventListener() {
-  const birthDateValue = birthDate.value;
   const luckyNumValue = luckyNum.value;
-  console.log("values", birthDateValue, luckyNumValue);
+  const valueOfDate = valueCheck(birthDate);
+  console.log(valueOfDate);
+
+  const sumOfDate = getSum(valueOfDate);
+  console.log(sumOfDate);
+
+  const checkOfLucky = checkLucky(sumOfDate, luckyNumValue);
+  console.log(checkOfLucky);
 }
 
 checkButton.addEventListener("click", () => checkEventListener());
